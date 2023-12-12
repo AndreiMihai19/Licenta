4558147d513bbb0d330d831bee89e1e029169405
AndreiMihai19 <andrei.mihai2@student.upt.ro>
2023-12-12 17:35:55 +0200

    Add panels

--- AdministratorApp/AdministratorApp/Form1.Designer.cs
+++ AdministratorApp/AdministratorApp/Form1.Designer.cs
@@ -38,10 +38,8 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
-            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -161,25 +159,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel6.BackColor = System.Drawing.Color.Crimson;
-            this.panel6.Controls.Add(this.label1);
             this.panel6.Location = new System.Drawing.Point(39, 19);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(1025, 102);
             this.panel6.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(361, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(330, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "STATUS ANGAJATI";
             // 
             // dashboard
             // 
@@ -192,8 +175,6 @@
             this.Text = "DASHBOARD";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
-            this.panel6.ResumeLayout(false);
-            this.panel6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -210,7 +191,6 @@
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Label label1;
     }
 }
 
